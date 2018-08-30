a27d818b528a307f269a5eb8eb8eb45cb401fad7
sungyull <sungyull@gmail.com>
2018-08-31 02:15:02 +0900

    Dojo FBV 반영

--- dojo/urls.py
+++ dojo/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     url(r'^sum/(?P<numbers>[\d/]+)/$', views.mysum),
     url(r'^hello/$', views.myname),
     url(r'^hello/(?P<name>[a-zA-Zㄱ-힣]+)/(?P<age>[\d]+)/$', views.myname),
+    url(r'^list1/$', views.post_list1),
+    url(r'^list2/$', views.post_list2),
+    url(r'^list3/$', views.post_list3),
+    url(r'^xls/$', views.excel_down),
 ]
--- dojo/views.py
+++ dojo/views.py
@@ -1,10 +1,46 @@
-from django.http import HttpResponse
+import os
+from django.conf import settings
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
+
 
 def mysum(request, numbers):
     # numbers = "1/12121/1212/1/21/21/21/2/12/1/2"
     rtn = sum(map(lambda s: int(s or 0), numbers.split("/")))
     return HttpResponse(rtn)
 
+
 def myname(request, name="""QQQ""", age=10):
     return HttpResponse( """{}, 안녕하세요. {}살 이네요?""".format(name, age))
+
+
+def post_list1(request):
+    name = '공유1'
+    return HttpResponse("""
+    <h1>Ask Django </h1>
+    <p>{name}</p>
+    <p>파이선 장고 중..... </p>    
+    """.format(name = name))
+
+
+def post_list2(request):
+    name = '공유2'
+    return render(request, 'dojo/post_list.html', {'name':name})
+
+
+def post_list3(request):
+    return JsonResponse({
+        'message':'안녕 파이썬 장고',
+        'items':['Python','장고','믄','AWS'],
+    }, json_dumps_params={'ensure_ascii':False})
+
+
+def excel_down(request):
+    #filepath = """D:/DJHome/askDjango/dojo/tests.xls"""
+    filepath = os.path.join(settings.BASE_DIR, 'tests.xls')
+    filename = os.path.basename(filepath)
+    with open(filepath, 'rb') as f:
+        response = HttpResponse(f, content_type='application/vnd.ms-excel')
+        # 필요한 응답헤더 세팅
+        response['Content-Disposition'] = 'attachment; filename="{}"'.format(filename)
+        return response
