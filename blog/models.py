89b26e55cee6458e00ffc6885566d2d05d300cb9
sungyull <sungyull@gmail.com>
2018-09-02 17:40:07 +0900

    EP6. Migrations 적용

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,23 @@
 #blog/models.py
+import re
 from django.db import models
+from django.forms import ValidationError
+from django.utils import timezone
 
+
+def lnglat_validator(value):
+    if not re.match(r'^([+-]?\d+\.?\d*),([+-]?\d+\.?\d*)$', value):
+        raise ValidationError('Invalid LngLat Type')
 
 class Post(models.Model):
+    author = models.CharField(max_length=50, verbose_name='작성자')
     title = models.CharField(max_length=100, verbose_name='제목',
                              help_text="""포스팅 제목을 입력해주세요. 최대 100자""")
     content = models.TextField(verbose_name='내용')
+    tags = models.CharField(max_length=100, blank=True)
+    lnglat = models.CharField(max_length=50, blank=True,
+        validators=[lnglat_validator],
+        help_text = '위도,경도')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
