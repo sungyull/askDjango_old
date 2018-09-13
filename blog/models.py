f9b5801b953ce66bc39078cc7484f80526f7e807
sungyull <sungyull@gmail.com>
2018-09-13 15:14:00 +0900

    중간저장

--- blog/models.py
+++ blog/models.py
@@ -52,7 +52,7 @@ class Comment(models.Model):
 class Tag(models.Model):
     name = models.CharField(max_length=50, unique=True)
 
-    def __str__(self):
+    def __str__(self):  
         return self.name
 
 
