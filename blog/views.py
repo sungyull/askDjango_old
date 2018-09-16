b3d80e1b73d279a7f3708b88e5d26a7d54d0ef33
sungyull <sungyull@gmail.com>
2018-09-16 16:18:16 +0900

    EP26. 스테틱파일 13분

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 # blog/views.py
-
+from django.contrib import messages
 from django.shortcuts import get_object_or_404,  render, redirect
 from .forms import PostForm
 from .models import Post
@@ -36,6 +36,7 @@ def post_new(request):
         form = PostForm(request.POST, request.FILES)
         if form.is_valid():
             post = form.save()
+            messages.success(request, '새 포스팅을 저장했습니다.')
             return redirect(post)  # post.get_absolute_url()
     else:
         form = PostForm()
@@ -51,6 +52,7 @@ def post_edit(request, id):
         form = PostForm(request.POST, request.FILES, instance=post)
         if form.is_valid():
             post = form.save()
+            messages.success(request, '포스팅을  수정했습니다.')
             return redirect(post)  # post.get_absolute_url()
     else:
         form = PostForm(instance=post)
